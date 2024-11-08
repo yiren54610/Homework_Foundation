ace213ba357c6053d65b63c7e202be65b4c1e990
yiren54610 <yl5732@columbia.edu>
2024-11-08 16:45:44 -0500

    PartOne

---
+++ # homework.py
@@ -0,0 +1,6 @@
+# homework-Part One
+doc_m='https://open-meteo.com/en/docs'
+
+url='https://api.open-meteo.com/v1/forecast?latitude=31.2222&longitude=121.4581&hourly=temperature_2m&timezone=America%2FNew_York&models=cma_grapes_global'
+
+pip install requests
